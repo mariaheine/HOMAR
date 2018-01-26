d472aa09b795ef3a6c1ddbddb7eb5f60a6284f09
BehemothTheCat <altanhir@gmail.com>
2018-01-26 21:07:59 +0100

    github pages test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Unity src="Build/build.json" loader="Build/UnityLoader.js" />
+        <Unity src="UnityBuild/build.json" loader="UnityBuild/UnityLoader.js" />
       </div>
     );
   }
