3a620e26ea138004facc7e21dfd4d969a27cf9fd
BehemothTheCat <altanhir@gmail.com>
2018-01-27 01:10:35 +0100

    Version 1.0.0 launch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import Unity from "react-unity-webgl";
 import "./App.css";
+// import "/../TemplateData/style.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Unity src="UnityBuild/build.json" loader="UnityBuild/UnityLoader.js" />
-      </div>
+      <Unity src="UnityBuild/build.json" loader="UnityBuild/UnityLoader.js" />
     );
   }
 }
