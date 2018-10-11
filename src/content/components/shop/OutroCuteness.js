c1829da0c3e0b1c4ce4a815f48e028d8278b89bc
psthmn-cnd <altanhir@gmail.com>
2018-10-11 11:57:11 +0200

    Kindof done OutroCuteness mainpage, we need another CSS dev tho

--- src/content/components/shop/OutroCuteness.js
+++ src/content/components/shop/OutroCuteness.js
@@ -1,39 +1,41 @@
 import React, { Component } from "react";
-import {
-  NavLink
-} from "reactstrap";
+import { NavLink } from "reactstrap";
 
 import outroproducts from "./../../../articles/products/outrocuteness.json";
+
+var tmpImg =
+  "https://s3.eu-central-1.amazonaws.com/homar/outrocuteness/mem1.jpg";
 
 // var products = JSON.parse(outroproducts);
 
 export default class OutroCuteness extends Component {
   render() {
-
     var finalprofucts = outroproducts.products.map(x => (
-        <CuteProduct id={x.id} />
+      <CuteProduct id={x.id} />
     ));
-    
-    var listedProducts = finalprofucts.map(x => (        
-        <li className="flex-item">{x}</li>
-    ))
-    
+
+    var listedProducts = finalprofucts.map(x => (
+      <li className="flex-item">{x}</li>
+    ));
+
     return (
       <div className="content">
-        <h1>OUTRO CUTENESS</h1>
-        <ul className="flex-container">
-            {listedProducts}
-        </ul>
+        <div className="productHeader" >
+          <h1 className="productHeader">Outro Cuteness</h1>
+        </div>
+        <ul className="flex-container">{listedProducts}</ul>
       </div>
     );
   }
 }
 
-const CuteProduct = (props) => {
-    return (
-        <NavLink href={`#/outrocuteness/${props.id}`} >
-            {props.id}
-            {/* <p>{outroproducts.products[props.id].memeSrc}</p> */}
-        </NavLink>
-    )
-}
+const CuteProduct = props => {
+  return (
+    <NavLink href={`#/outrocuteness/${props.id}`}>
+      <div className="listedOutro">
+        {/* {props.id} */}
+        <img className="listedOutro" alt="" src={tmpImg} />
+      </div>
+    </NavLink>
+  );
+};
