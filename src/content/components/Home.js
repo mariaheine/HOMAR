c4e546fd4dce91ead6e3b1d0531cc1011b96538d
psthmn-cnd <altanhir@gmail.com>
2018-10-11 13:52:35 +0200

    Okay, added basic Aframe scene for the homepage

--- src/content/components/Home.js
+++ src/content/components/Home.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "aframe";
+import { Entity, Scene } from "aframe-react";
+import "aframe-environment-component";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <p>
-            asd asd
-        </p>
+      <div className="aframeContainer">
+        <Scene embedded vr-mode-ui="enabled: false">
+          <Entity environment="preset: yavapai;" />
+          <Entity
+            position="0 2 -5"
+            geometry="primitive: plane; height: 2; width: 4;"
+            material={{color: 'white', opacity: 0.3, side: 'double', shader: 'flat'}}
+            text={{color: "#000", anchor: "center", width: "3.5", value: "7awOWSP6szXgHZrOZGaKMPQBJ28wZ9HQgCOKskkqRRm5cV3vbbA+ZK2zJQVZfjDrwZKUs49exVHBMLOsD8bYXEEXFOLGZbjTgUHslI8dedOLseB+DcOb7XNoCXod6+/fCdmFDdd7QEpSDSA9M+MjhGlXIZvUtYDTuNDN4tK/PwD9MTrc8ATzCWMoTLWrQHSphuKuOcr5DEjc5STAaM4PHsL0RK41dOI/khN589Eoa5OMNhDpePl+OGXJJioPpeYrwcezVUDQ0rJi6RACXUAc6Q=="}}
+          />
+          <Entity camera position="0 0.1 0" wasd-controls look-controls />
+        </Scene>
       </div>
-    )
+    );
   }
 }
