cfd7d1c37c421abca942a38b994820ef9e9335ba
psthmn-cnd <altanhir@gmail.com>
2018-10-11 12:17:36 +0200

    Outro Cuteness done

--- src/content/components/shop/OutroProduct.js
+++ src/content/components/shop/OutroProduct.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import {
-  NavLink
-} from "reactstrap";
+import { NavLink } from "reactstrap";
 
 import outroproducts from "./../../../articles/products/outrocuteness.json";
 
 var str = JSON.stringify(outroproducts, null, 2);
 
-export default class OutroProduct extends Component {  
+export default class OutroProduct extends Component {
   render() {
     console.log("=======================================");
     console.log("ss: ", this.props.match.params.productId);
@@ -20,15 +18,25 @@ export default class OutroProduct extends Component {
     // OLD:
     // var paragraphs = product.description.map(x => <p>{x}</p>);
     // NEW: Why is id "dangerous"?
-    var paragraphs = product.description.map(x => <p dangerouslySetInnerHTML={{__html: `${x}`}} />);
+    var paragraphs = product.description.map(x => (
+      <p dangerouslySetInnerHTML={{ __html: `${x}` }} />
+    ));
 
     // var header = product.header.map(x => <p className="flex-item outro">{x}</p>);
     // var header = <p className="flex-item outro header">{product.header}</p>;
-    var header = <p id="outroHeader" dangerouslySetInnerHTML={{__html: `${product.header}`}}/>;
+    var header = (
+      <NavLink href="#/outrocuteness">
+        <p
+          id="outroHeader"
+          dangerouslySetInnerHTML={{ __html: `${product.header}` }}
+        />
+      </NavLink>
+    );
+
     var imageSrc = `https://s3.eu-central-1.amazonaws.com/homar/outrocuteness/mem${id}.jpg`;
 
-    var tmpImg = "https://s3.eu-central-1.amazonaws.com/homar/outrocuteness/mem1.jpg";
-
+    var tmpImg =
+      "https://s3.eu-central-1.amazonaws.com/homar/outrocuteness/mem1.jpg";
 
     return (
       <div className="flex-container outro">
@@ -38,8 +46,12 @@ export default class OutroProduct extends Component {
         </div>
         <div id="outroDescription" className="flex-item outro">
           {paragraphs}
-          <NavLink className="" href="#/outrocuteness">Back to shop</NavLink>  
-          <NavLink className="" href="#/">Buy</NavLink>
+          <NavLink className="" href="#/outrocuteness">
+            Back to shop
+          </NavLink>
+          <NavLink className="" href="#/">
+            Buy
+          </NavLink>
         </div>
       </div>
     );
