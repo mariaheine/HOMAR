462cb51290b01eb44a90c26938d8ffc0624efe81
psthmn-cnd <altanhir@gmail.com>
2018-12-04 12:33:41 +0100

    Firebase init

---
+++ src/content/config/firebaseConfig.js
@@ -0,0 +1,20 @@
+// React, Redux & Firebase App Tutorial #15 - Creating a Firebase Project
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAlQtvOiTGGcPZoztAoLKhUfgn3gB1dB4I",
+  authDomain: "homar-webapp.firebaseapp.com",
+  databaseURL: "https://homar-webapp.firebaseio.com",
+  projectId: "homar-webapp",
+  storageBucket: "homar-webapp.appspot.com",
+  messagingSenderId: "1052257630582"
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
