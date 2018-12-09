81eb27cb2f692cd3deaf9792a740fca531f2822a
psthmn-cnd <altanhir@gmail.com>
2018-12-09 16:19:08 +0100

    Before firebase authentication

--- src/reduxStore/reducers/rootReducer.js
+++ src/reduxStore/reducers/rootReducer.js
@@ -4,12 +4,14 @@ import langReducer from "./langReducer";
 
 import { combineReducers } from "redux";
 import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase"
 
 const rootReducer = combineReducers({
   auth: authReducer,
   post: postReducer,
   language: langReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer;
